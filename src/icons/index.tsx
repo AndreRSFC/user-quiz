f2380fae970794280a89fd36bea915ffb4a6502c
Andre <andrersfcosta@gmail.com>
2024-05-18 19:10:14 -0300

    feat: configuring svgr and add icons

---
+++ src/icons/index.tsx
@@ -0,0 +1,4 @@
+export { default as Logo } from './logo.svg';
+export { default as Twitter } from './twitter.svg';
+export { default as Google } from './google.svg';
+export { default as Facebook } from './facebook.svg';
